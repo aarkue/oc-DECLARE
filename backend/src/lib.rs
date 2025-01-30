ace3608f147e8329e66cbc7ba969f30cc3481702
aarkue <git@aarkue.eu>
2025-01-30 17:39:20 +0100

    Better any(...) handling

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, hash::Hash};
+use std::{borrow::Cow, collections::HashSet, hash::Hash};
 
 use itertools::{Itertools, MultiProduct};
 use process_mining::ocel::{
@@ -14,12 +14,12 @@ enum OCDeclareNode {
     ObjectEnd(String),
 }
 
-impl<'a> Into<&'a str> for &'a OCDeclareNode {
-    fn into(self) -> &'a str {
+impl<'a> Into<Cow<'a, String>> for &'a OCDeclareNode {
+    fn into(self) -> Cow<'a, String> {
         match self {
-            OCDeclareNode::Activity(a) => a.as_str(),
-            OCDeclareNode::ObjectInit(_) => todo!(),
-            OCDeclareNode::ObjectEnd(_) => todo!(),
+            OCDeclareNode::Activity(a) => Cow::Borrowed(a),
+            OCDeclareNode::ObjectInit(ot) => Cow::Owned(format!("<init> {ot}")),
+            OCDeclareNode::ObjectEnd(ot) => Cow::Owned(format!("<exit> {ot}")),
         }
     }
 }
@@ -44,40 +44,102 @@ struct OCDeclareArc {
     arc_type: OCDeclareArcType,
     label: OCDeclareArcLabel,
 }
-// use rayon::prelude::*;
+use rayon::prelude::*;
 impl<'a> OCDeclareArc {
-    pub fn get_for_all_evs(&self,linked_ocel:&LinkedOCEL<'_>) -> Vec<Vec<usize>> {
-        linked_ocel.events_per_type.get(Into::<&str>::into(&self.from)).unwrap().
-        // par_iter()
-        iter()
-        .map(|ev| {
-            self.get_for_ev(ev, linked_ocel)
-        }).collect()
+    pub fn get_for_all_evs(&self, linked_ocel: &LinkedOCEL<'_>) -> Vec<Vec<usize>> {
+        linked_ocel
+            .events_per_type
+            .get(Into::<Cow<_>>::into(&self.from).as_str())
+            .unwrap()
+            .par_iter()
+            // iter()
+            .map(|ev| self.get_for_ev(ev, linked_ocel))
+            .collect()
     }
-    pub fn get_for_ev(&self, ev: &'a OCELEvent, linked_ocel:&LinkedOCEL<'_> ) -> Vec<usize> {
-        self.label.get_bindings(ev, linked_ocel).map(|binding| {
-            let binding = binding.collect_vec();
-            // Now get the number of events fulfilling this binding criteria
-            // linked_ocel.events_per_type.get(Into::<&str>::into(&self.to)).unwrap().iter().filter(|e| {
-            //     let obs: HashSet<ObjectID> = linked_ocel.get_ev_rels(&e.id).unwrap().iter().map(|x| (&x.1.id).into()).collect();
-            //     // println!("{:?}",obs);
-            //     binding.iter().all(|b| b.check(&obs))
-            // false
-            // }).count()
-            get_evs_with_objs(&binding,linked_ocel,Into::<&str>::into(&self.to)).len()
-            // binding.len()
-        }).collect_vec()
+    pub fn get_for_ev(&self, ev: &'a OCELEvent, linked_ocel: &LinkedOCEL<'_>) -> Vec<usize> {
+        self.label
+            .get_bindings(ev, linked_ocel)
+            .map(|binding| {
+                let binding = binding.collect_vec();
+                // println!("{:?}",binding);
+                // Now get the number of events fulfilling this binding criteria
+                // linked_ocel.events_per_type.get(Into::<&str>::into(&self.to)).unwrap().iter().filter(|e| {
+                //     let obs: HashSet<ObjectID> = linked_ocel.get_ev_rels(&e.id).unwrap().iter().map(|x| (&x.1.id).into()).collect();
+                //     // println!("{:?}",obs);
+                //     binding.iter().all(|b| b.check(&obs))
+                // false
+                // }).count()
+                get_evs_with_objs(
+                    &binding,
+                    linked_ocel,
+                    Into::<Cow<_>>::into(&self.to).as_str(),
+                )
+                .len()
+                // binding.len()
+            })
+            .collect_vec()
     }
 }
 
-fn get_evs_with_objs<'a>(objs: &Vec<SetFilter<ObjectID<'_>>>,  linked_ocel:&'a LinkedOCEL<'_>, etype: &'a str) -> Vec<EventID<'a>> {
+fn get_evs_with_objs<'a>(
+    objs: &Vec<SetFilter<ObjectID<'_>>>,
+    linked_ocel: &'a LinkedOCEL<'_>,
+    etype: &'a str,
+) -> Vec<EventID<'a>> {
     let mut initial: Vec<EventID> = match &objs[0] {
-        SetFilter::Any(_items) => linked_ocel.events_per_type.get(etype).unwrap().iter().map(|e| (&e.id).into()).collect(),
-        SetFilter::All(items) => linked_ocel.e2o_rel_rev.get(&items[0]).unwrap().into_iter().filter(|(_,e)| items.iter().skip(1).all(|o| e.relationships.iter().any(|r| Into::<ObjectID>::into(&r.object_id) == *o))).map(|x| (&x.1.id).into()).collect_vec(),
+        SetFilter::Any(items) =>
+        //  linked_ocel
+        //     .events_per_type
+        //     .get(etype)
+        //     .unwrap()
+        //     .iter()
+        {
+            items
+                .iter()
+                .flat_map(|o| {
+                    linked_ocel
+                        .e2o_rel_rev
+                        .get(o)
+                        .unwrap()
+                        .iter()
+                        .map(|e| e.1)
+                        .filter(|e| e.event_type == *etype)
+                })
+                .map(|e| (&e.id).into())
+                .collect()
+        }
+        SetFilter::All(items) => {
+            if items.len() == 0 {
+                return Vec::new();
+            }
+            // items.iter().flat_map(|o| linked_ocel.e2o_rel_rev.get(o).unwrap().iter().map(|e| e.1).filter(|e| e.event_type == *etype))
+            linked_ocel
+                .e2o_rel_rev
+                .get(&items[0])
+                .unwrap()
+                .into_iter()
+                .filter(|(_, e)| {
+                    e.event_type == etype
+                        && items.iter().skip(1).all(|o| {
+                            e.relationships
+                                .iter()
+                                .any(|r| Into::<ObjectID>::into(&r.object_id) == *o)
+                        })
+                })
+                .map(|x| (&x.1.id).into())
+                .collect_vec()
+        }
     };
+    // println!("Initial is of size: {}",initial.len());
     for o in objs.iter() {
         initial.retain(|e| {
-            let obs = linked_ocel.e2o_rel.get(e).unwrap().iter().map(|o| ObjectID::from(&o.1.id)).collect();
+            let obs = linked_ocel
+                .e2o_rel
+                .get(e)
+                .unwrap()
+                .iter()
+                .map(|o| ObjectID::from(&o.1.id))
+                .collect();
             o.check(&obs)
         });
     }
@@ -138,7 +200,35 @@ impl ObjectTypeAssociation {
                 first,
                 second,
                 reversed,
-            } => todo!(),
+            } => linked_ocel
+                .get_ev_rels(ev)
+                .unwrap()
+                .iter()
+                .map(|x| x.1)
+                .filter(|o| o.object_type == *first)
+                .flat_map(|o| {
+                    if !reversed {
+                        linked_ocel
+                            .o2o_rel
+                            .get(&Into::<ObjectID>::into(&o.id))
+                            .unwrap()
+                            .iter()
+                            .map(|rel| rel.1)
+                            .filter(|o2| o2.object_type == *second)
+                            .collect_vec()
+                    } else {
+                        linked_ocel
+                            .o2o_rel_rev
+                            .get(&Into::<ObjectID>::into(&o.id))
+                            .unwrap()
+                            .iter()
+                            .map(|rel| rel.1)
+                            .filter(|o2| o2.object_type == *second)
+                            .collect_vec()
+                    }
+                })
+                .map(|o| ObjectID::from(&o.id))
+                .collect(),
         }
     }
 }
@@ -157,12 +247,12 @@ enum SetFilter<T: Eq + Hash> {
 }
 
 impl<T: Eq + Hash> SetFilter<T> {
- pub fn check(&self, s: &HashSet<T>) -> bool {
-    match self {
-        SetFilter::Any(items) => items.iter().any(|i| s.contains(i)),
-        SetFilter::All(items) => items.iter().all(|i| s.contains(i)),
+    pub fn check(&self, s: &HashSet<T>) -> bool {
+        match self {
+            SetFilter::Any(items) => items.iter().any(|i| s.contains(i)),
+            SetFilter::All(items) => items.iter().all(|i| s.contains(i)),
+        }
     }
- }
 }
 
 impl<'a> OCDeclareArcLabel {
@@ -179,15 +269,22 @@ impl<'a> OCDeclareArcLabel {
                 self.all
                     .iter()
                     .map(|otass| SetFilter::All(otass.get_for_ev(&ev.id, linked_ocel)))
-                    .chain(vec![SetFilter::All(product)].into_iter())
+                    .chain(
+                        if product.is_empty() {
+                            Vec::default()
+                        } else {
+                            vec![SetFilter::All(product)]
+                        }
+                        .into_iter(),
+                    )
                     .chain(
                         self.any
                             .iter()
                             .map(|otass| SetFilter::Any(otass.get_for_ev(&ev.id, linked_ocel))),
                     )
-                    // .collect_vec()
+                // .collect_vec()
             })
-            // .collect_vec()
+        // .collect_vec()
     }
 }
 
@@ -201,33 +298,49 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let ocel =
-            import_ocel_json_from_path("/home/aarkue/dow/ocel/order-management.json").unwrap();
+        // let ocel =
+        // import_ocel_json_from_path("/home/aarkue/dow/ocel/order-management.json").unwrap();
+        let ocel = import_ocel_json_from_path(
+            "/home/aarkue/dow/ocel/bpic2017-o2o-workflow-qualifier.json",
+        )
+        .unwrap();
         let linked_ocel: OwnedLinkedOcel = ocel.into();
+        // let x = OCDeclareArc {
+        //     from: OCDeclareNode::new_act("place order"),
+        //     to: OCDeclareNode::new_act("place order"),
+        //     arc_type: OCDeclareArcType::EF,
+        //     label: OCDeclareArcLabel {
+        //         each: vec![ObjectTypeAssociation::new_simple("customers")],
+        //         all: vec![ObjectTypeAssociation::new_simple("products"),ObjectTypeAssociation::new_simple("items")],
+        //         any: vec![ObjectTypeAssociation::new_o2o_rev("orders", "customers")],
+        //         ..Default::default()
+        //     },
+        // };
+
         let x = OCDeclareArc {
-            from: OCDeclareNode::new_act("place order"),
-            to: OCDeclareNode::new_act("payment reminder"),
+            from: OCDeclareNode::new_act("A_Accepted"),
+            to: OCDeclareNode::new_act("O_Accepted"),
             arc_type: OCDeclareArcType::EF,
             label: OCDeclareArcLabel {
-                each: vec![ObjectTypeAssociation::new_simple("orders")],
+                any: vec![ObjectTypeAssociation::new_o2o("Application", "Offer")],
                 ..Default::default()
             },
         };
-        // let ev = linked_ocel.ocel_ref().events.iter().filter(|e| e.event_type == "place order").next().unwrap();
-        // let res = x.get_for_ev(ev, &linked_ocel.linked_ocel);
-        // println!("{:?}",res);
-
-
         let now = Instant::now();
         let all_res = x.get_for_all_evs(&linked_ocel.linked_ocel);
         println!("Took {:?}", now.elapsed());
-        println!("{:?}",all_res.iter().take(10).collect_vec());
+        println!("{:?}", all_res.iter().take(10).collect_vec());
+        
+        let count: usize = all_res.iter().flatten().sum();
+        
+        
+        let at_least_one: usize = all_res.iter().flatten().filter(|r| **r >= 1).count();
+        
+        println!("Len: {}",all_res.len());
+        println!("Count: {count}");
+        println!("At least one: {}",at_least_one);
+        // println!("{}", serde_json::to_string_pretty(&x).unwrap());
 
-        // for y in x.label.get_bindings(linked_ocel.ocel_ref().events.iter().filter(|e| e.event_type == "confirm order").next().unwrap(), &linked_ocel.linked_ocel) {
-        //     for yy in y {
-        //         println!("{:?}",yy);
-        //     }
-        // }
-        println!("{:?}", x);
+        // println!("{:?}", x);
     }
 }
