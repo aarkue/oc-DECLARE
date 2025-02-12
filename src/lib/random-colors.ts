e3415d8c9aadc2f1ca36af04e912c201f86a6c18
aarkue <git@aarkue.eu>
2025-02-12 11:04:19 +0100

    Hardcode default colors for specific object types

--- src/lib/random-colors.ts
+++ src/lib/random-colors.ts
@@ -1,6 +1,19 @@
 export function getRandomStringColor(s: string, saturation = 80, lightness = 50) {
     // return '#292d2a';
     // console.log(s);
+    // Demo mode:
+    if(s === "item"){
+        return "#ff9358"
+    }
+    if(s === "employee"){
+        return "#fb7fe1"
+    }
+    if(s === "order"){
+        return "#529ad1"
+    }
+    if(s === "customer"){
+        return "#69bbac"
+    }
     let h =  14;
     for(let i = 0; i < s.length; i++){
         h = Math.imul(31, h) + (s.charCodeAt(i)) | 0;
