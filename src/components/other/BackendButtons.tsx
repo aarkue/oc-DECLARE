d41bbdf0b21c199932dacad8d137d3dbd274e6d2
aarkue <git@aarkue.eu>
2025-02-27 21:34:31 +0100

    Extract WASM calls to worker to not block main thread

--- src/components/other/BackendButtons.tsx
+++ src/components/other/BackendButtons.tsx
@@ -1,21 +1,49 @@
 import { CustomEdge } from "@/edges/types";
 import { ActivityNode } from "@/nodes/types";
 import { useEdges, useReactFlow } from "@xyflow/react";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import init, { discover_oc_declare_constraints, get_all_edge_violation_percentage_perf, get_ot_act_involvements, initThreadPool, load_ocel_json, load_ocel_xml, unload_ocel } from "../../../crates/backend-wasm/pkg/backend_wasm";
 import type { OCDeclareArc } from "../../../crates/shared/bindings/OCDeclareArc";
 
-import { OCELInfoContext } from "@/lib/ocel-info";
+import { OCELInfo, OCELInfoContext } from "@/lib/ocel-info";
 import { addArcsToFlow, flowEdgeToOCDECLARE } from "@/lib/type-conversions";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
+import WASMWorker from "../../lib/worker?worker"
+const worker = new WASMWorker();
 export default function BackendButton() {
     const inputRef = useRef<HTMLInputElement>(null);
     const flow = useReactFlow<ActivityNode, CustomEdge>();
     const selectedEdges = useEdges<CustomEdge>().filter(e => e.selected)
     const [status, setStatus] = useState<"initial" | "ocel-loaded">("initial")
     const { setOcelInfo } = useContext(OCELInfoContext);
+// TODO: Extract types & Add error handling
+    useEffect(() => {
+        const messageListener = (e: MessageEvent<{ type: "ocel-loaded", info: OCELInfo } | { type: "ocel-unloaded" } | { type: "discovered", discoveredArcs: OCDeclareArc[] } | { type: "edges-evaluated", edgeIDs: string[], violFracs: number[] } | { type: "" }>) => {
+            if (e.data.type === "ocel-loaded") {
+                setOcelInfo(e.data.info);
+                setStatus("ocel-loaded");
+            } else if (e.data.type === "ocel-unloaded") {
+                setStatus("initial");
+                setOcelInfo({});
+            } else if (e.data.type === "discovered") {
+                addArcsToFlow(e.data.discoveredArcs, flow);
+                console.log(e.data.discoveredArcs);
+            } else if (e.data.type === "edges-evaluated") {
+                for (let i = 0; i < e.data.edgeIDs.length; i++) {
+                    flow.updateEdgeData(e.data.edgeIDs[i], { violationInfo: { violationPercentage: 100 * e.data.violFracs[i] } });
+                }
+            } else {
+                console.warn("Unknown message type: " + e.data.type);
+            }
+        };
+        worker.addEventListener("message", messageListener);
+        return () => {
+            worker.removeEventListener("message", messageListener);
+        }
+    })
+
     return <>
         {status === "initial" && <Input type="file" ref={inputRef} className="max-w-[7rem]" />}
         {status === "initial" && <Button onClick={async () => {
@@ -27,44 +55,36 @@ export default function BackendButton() {
                     console.log("Thread pool error: ", e);
                 }
                 const file = inputRef.current?.files[0];
-                // const y = await file.bytes()
-                const ocelFileData = await file.arrayBuffer()
-                const x = new Uint8Array(ocelFileData);
-                console.log(x.length)
-                file.name.endsWith(".json") ? load_ocel_json(x) : load_ocel_xml(x)
-                // console.log("Got ocel pointer: " + ocelRef.current);
-                setStatus("ocel-loaded");
-                const otActInvolvement = JSON.parse(get_ot_act_involvements());
-                console.log(otActInvolvement);
-                setOcelInfo(otActInvolvement);
+                worker.postMessage({ type: "load-ocel", file });
+                // // const y = await file.bytes()
+                // const ocelFileData = await file.arrayBuffer()
+                // const x = new Uint8Array(ocelFileData);
+                // console.log(x.length)
+                // file.name.endsWith(".json") ? load_ocel_json(x) : load_ocel_xml(x)
+                // // console.log("Got ocel pointer: " + ocelRef.current);
+                // setStatus("ocel-loaded");
+                // const otActInvolvement = JSON.parse(get_ot_act_involvements());
+                // console.log(otActInvolvement);
+                // setOcelInfo(otActInvolvement);
             }
         }}>Load</Button>}
         {status === "ocel-loaded" && <Button variant="destructive" onClick={() => {
-            try {
-
-                unload_ocel();
-            } finally {
-                setStatus("initial");
-                setOcelInfo({});
-            }
+            worker.postMessage({ type: "unload-ocel" })
         }} >
             Unload</Button>}
         {status === "ocel-loaded" &&
             <><Button onClick={async () => {
-                const beginning = Date.now();
                 const edges = (selectedEdges.length > 0 ? selectedEdges : flow.getEdges());
-                const edgeJSON = JSON.stringify(edges.map(e => flowEdgeToOCDECLARE(e, flow)));
-                const violationFracs = get_all_edge_violation_percentage_perf(edgeJSON);
-                for (let i = 0; i < edges.length; i++) {
-                    flow.updateEdgeData(edges[i].id, { violationInfo: { violationPercentage: 100 * violationFracs[i] } });
+                const edgeIDs = edges.map(e => e.id);
+                const edgesConverted = edges.map(e => flowEdgeToOCDECLARE(e, flow));
+                worker.postMessage({type: "evaluate-edges", edges: edgesConverted, edgeIDs})
+                // const violationFracs = get_all_edge_violation_percentage_perf(edgeJSON);
 
-                }
                 // edges.forEach(e => {
                 //     const x = flowEdgeToOCDECLARE(e, flow);
                 //     const violFrac = getEdgeViolationPerc(x);
                 //     console.log(violFrac)
                 // });
-                console.log("TOTAL Evaluation took " + ((Date.now() - beginning) / 1000) + "s");
             }}>
                 Evaluate {selectedEdges.length === 0 ? "All" : ""}
             </Button>
@@ -77,12 +97,7 @@ export default function BackendButton() {
         {status === "ocel-loaded" &&
             <Button onClick={async () => {
                 try {
-                    let now = Date.now();
-                    const res = discover_oc_declare_constraints(0.4);
-                    console.log("Discovery took " + ((Date.now() - now) / 1000) + "s");
-                    const discoverdArcs: OCDeclareArc[] = JSON.parse(res);
-                    addArcsToFlow(discoverdArcs, flow);
-                    console.log(discoverdArcs);
+                    worker.postMessage({ type: "discover" });
                 } catch (e) {
                     console.error(e);
                 }
---
+++ src/lib/worker.ts
@@ -0,0 +1,48 @@
+
+import { OCDeclareArc } from "crates/shared/bindings/OCDeclareArc";
+import init, { discover_oc_declare_constraints, get_all_edge_violation_percentage_perf, get_ot_act_involvements, initThreadPool, load_ocel_json, load_ocel_xml, unload_ocel } from "../../crates/backend-wasm/pkg/backend_wasm";
+
+// listen for messages from UI thread
+onmessage = function (e: MessageEvent<{ type: "load-ocel", file: File } | { type: "unload-ocel" } | { type: "discover" } | { type: "evaluate-edges", edges: OCDeclareArc[], edgeIDs: string[] } | { type: "" }>) {
+    if (e.data.type === "load-ocel") {
+        const data = e.data;
+        // read contents of file
+        const reader = new FileReader();
+        reader.onload = async () => {
+            const x = new Uint8Array(reader.result as ArrayBuffer);
+
+            await init();
+            try {
+                await await initThreadPool(Math.max(1, Math.round(0.25 * navigator.hardwareConcurrency)));
+            } catch (e) {
+                console.log("Thread pool error: ", e);
+            }
+            data.file.name.endsWith(".json") ? load_ocel_json(x) : load_ocel_xml(x)
+            // console.log("Got ocel pointer: " + ocelRef.current);
+            // setStatus("ocel-loaded");
+            const otActInvolvement = JSON.parse(get_ot_act_involvements());
+
+            this.postMessage({ type: "ocel-loaded", info: otActInvolvement });
+        };
+        reader.readAsArrayBuffer(e.data.file);
+    } else if (e.data.type === "unload-ocel") {
+        unload_ocel();
+        this.postMessage({ type: "ocel-unloaded" });
+    } else if (e.data.type === "discover") {
+
+        let now = Date.now();
+        const res = discover_oc_declare_constraints(0.2);
+        const discoveredArcs: OCDeclareArc[] = JSON.parse(res);
+        console.log("Discovery took " + ((Date.now() - now) / 1000) + "s");
+        this.postMessage({ type: "discovered", discoveredArcs });
+    } else if (e.data.type === "evaluate-edges") {
+        const edgeJSON = JSON.stringify(e.data.edges);
+        const beginning = Date.now();
+        const violFracs = get_all_edge_violation_percentage_perf(edgeJSON);
+        console.log("TOTAL Evaluation took " + ((Date.now() - beginning) / 1000) + "s");
+        this.postMessage({ type: "edges-evaluated", edgeIDs: e.data.edgeIDs, violFracs })
+    }
+    else {
+        console.warn("Unknown message type: " + e.data.type);
+    }
+};
